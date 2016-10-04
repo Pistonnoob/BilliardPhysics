b175049b232fdb8e3aac9c441aeaaf8bc1d8d257
Pistonnoob <kimeld@hotmail.com>
2016-10-04 12:02:58 +0200

    +Constants and Ball struct v1

--- SmallGameProject/SmallGameProject/BilliardState.h
+++ SmallGameProject/SmallGameProject/BilliardState.h
@@ -2,6 +2,7 @@
 #define BILLIARDSTATE_H
 #include "GameState.h"
 #include "Model.h"
+#include "MyMathLib.h"
 class BilliardState :
 	public GameState
 {
@@ -11,11 +12,23 @@ private:
 	Model m_ballCue;
 	Model m_ball8;
 	Model m_cueStick; //Cue stick
+	//The pelt is made of velvet
 	Model m_table;
 
 	CameraHandler myCamera;
 
 	bool ballsNotMoving;
+	const float PI = DirectX::XM_PI;
+	const float FRICTIONCOEFFICIENT_BETWEEN_BALL_AND_VELVET = 0.005f;
+	const float GRAVITY = 9.82;
+
+	struct Ball {
+		DirectX::XMFLOAT3 pos;
+		DirectX::XMFLOAT3 velocity;
+		float density;
+		float mass;
+		float radius;
+	};
 
 public:
 	BilliardState();
